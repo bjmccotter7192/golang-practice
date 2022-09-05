6ddfc50588bf63c555809a3e24f916e837b197ef
BJ McCotter <bjmccotter7192@gmail.com>
2022-09-05 12:01:52 -0500

    temp checking in some code

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module nygiants-golang
+module golang-practice
 
-go 1.17
+go 1.18
--- main.go
+++ main.go
@@ -6,4 +6,11 @@ import (
 
 func main(){
 	fmt.Println("Testing this out")
+
+	myNum := 15
+	myNum2 := 16
+
+	total := myNum * myNum2
+
+	fmt.Println(total);
 }
