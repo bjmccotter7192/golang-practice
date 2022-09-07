7dd4907b0072a1e5e0ec44ded5cdbab4432e2bd6
BJ McCotter <bjmccotter7192@gmail.com>
2022-09-07 17:10:43 -0500

    uncommenting main.go

--- main.go
+++ main.go
@@ -1,44 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"golang-practice/bjmac"
 )
 
 func main() {
-	// /*
-	// 	This section covers Variables, values, and type
-	// */
-	// bjmac.LearnTheBasics() //Just playing around with the basics
-	// bjmac.NinjaExercise1() // First hands on example of Ninja/Jedi exercises
-	// bjmac.NinjaExercise2() // Second hands on example of Ninja/Jedi exercies
-	// bjmac.NinjaExercise3() // Third hands on example of Ninja/Jedi exercies
-	// bjmac.NinjaExercise4() // Forth hands on example of Ninja/Jedi exercies
+	/*
+		This section covers Variables, values, and type
+	*/
+	bjmac.LearnTheBasics() //Just playing around with the basics
+	bjmac.NinjaExercise1() // First hands on example of Ninja/Jedi exercises
+	bjmac.NinjaExercise2() // Second hands on example of Ninja/Jedi exercies
+	bjmac.NinjaExercise3() // Third hands on example of Ninja/Jedi exercies
+	bjmac.NinjaExercise4() // Forth hands on example of Ninja/Jedi exercies
 
-	// fmt.Println("================================================")
+	fmt.Println("================================================")
 
-	// /*
-	// 	This section covers programming fundamentals
-	// */
-	// bjmac.BoolTypePractice()
-	// bjmac.WorkingWithInts()
-	// bjmac.RuntimePlayground()
-	// bjmac.PlayingWithStrings()
-	// bjmac.NumeralSystemsPlayground()
-	// bjmac.BitShiftingFun()
+	/*
+		This section covers programming fundamentals
+	*/
+	bjmac.BoolTypePractice()
+	bjmac.WorkingWithInts()
+	bjmac.RuntimePlayground()
+	bjmac.PlayingWithStrings()
+	bjmac.NumeralSystemsPlayground()
+	bjmac.BitShiftingFun()
 
-	// fmt.Println("================================================")
+	fmt.Println("================================================")
 
-	// /*
-	// 	NINJA Level 2 Hands On Exercises
-	// */
-	// bjmac.NinjaLevel_2_Exercise_1()
-	// bjmac.NinjaLevel_2_Exercise_2()
-	// bjmac.NinjaLevel_2_Exercise_3()
-	// bjmac.NinjaLevel_2_Exercise_4()
-	// bjmac.NinjaLevel_2_Exercise_5()
-	// bjmac.NinjaLevel_2_Exercise_6()
+	/*
+		NINJA Level 2 Hands On Exercises
+	*/
+	bjmac.NinjaLevel_2_Exercise_1()
+	bjmac.NinjaLevel_2_Exercise_2()
+	bjmac.NinjaLevel_2_Exercise_3()
+	bjmac.NinjaLevel_2_Exercise_4()
+	bjmac.NinjaLevel_2_Exercise_5()
+	bjmac.NinjaLevel_2_Exercise_6()
 
-	// fmt.Println("================================================")
+	fmt.Println("================================================")
 
 	bjmac.ControlFlowFun()
 	bjmac.NinjaLevel_3_Exercise_1()
